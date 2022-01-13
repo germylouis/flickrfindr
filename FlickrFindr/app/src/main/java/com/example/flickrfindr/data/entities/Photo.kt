62ed49effded0bbc5cee00d93c96300f3b746f66
Germy Louis <glouis@imobile3.com>
2022-01-12 19:46:47 -0700

    Add data classes.

---
+++ FlickrFindr/app/src/main/java/com/example/flickrfindr/data/entities/Photo.kt
@@ -0,0 +1,34 @@
+/*
+Copyright (c) 2022 Kotlin Data Classes Generated from JSON powered by http://www.json2kotlin.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar */
+package com.example.flickrfindr.data.entities
+
+import kotlinx.serialization.SerialName
+
+data class Photo(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("owner")
+    val owner: String,
+    @SerialName("secret")
+    val secret: String,
+    @SerialName("server")
+    val server: Int,
+    @SerialName("farm")
+    val farm: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("ispublic")
+    val ispublic: Int,
+    @SerialName("isfriend")
+    val isfriend: Int,
+    @SerialName("isfamily")
+    val isfamily: Int
+)
