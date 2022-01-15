4ecf44e3207c018697750a4a8c18295ea9f6b5bb
Germy Louis <glouis@imobile3.com>
2022-01-15 12:32:13 -0700

    Add ui layer w/ rView and adapter in main FlickrActivity.

--- FlickrFindr/app/src/main/java/com/example/flickrfindr/FlickrApp.kt
+++ FlickrFindr/app/src/main/java/com/example/flickrfindr/FlickrApp.kt
@@ -6,30 +6,46 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.flickrfindr.data.datasources.BasePhotoDataSource
+import com.example.flickrfindr.data.entities.BasePhoto
 import com.example.flickrfindr.data.repos.BasePhotosRepos
 import com.example.flickrfindr.data.repos.PhotosRepos
+import com.example.flickrfindr.databinding.FlickrActivityMainBinding
+import com.example.flickrfindr.ui.adapters.FlickrPhotoAdapter
 import com.example.flickrfindr.viewmodels.PhotosViewModel
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 
 @InternalCoroutinesApi
 class FlickrApp : AppCompatActivity() {
-
+    private var flickrActivityMainBinding: FlickrActivityMainBinding? = null
+    private var photos: BasePhoto? = null
     private val photosRepos: PhotosRepos
         get() = BasePhotosRepos(BasePhotoDataSource())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        flickrActivityMainBinding = FlickrActivityMainBinding.inflate(layoutInflater)
+        setContentView(flickrActivityMainBinding?.root)
 
         val factory: ViewModelProvider.AndroidViewModelFactory = PhotosViewModel.Factory(application, photosRepos)
         val viewModel: PhotosViewModel by viewModels { factory }
+        val manager = LinearLayoutManager(this)
+
         lifecycleScope.launchWhenCreated {
             viewModel.getPhotos().collect {
                 Log.d("germ", "onCreate: ${it?.photos?.total}")
+                photos = it
+            }
+            flickrActivityMainBinding?.flickRv?.apply {
+                layoutManager = manager
+                adapter = FlickrPhotoAdapter(photos)
+
             }
         }
 
     }
+
 }
+
--- FlickrFindr/app/src/main/java/com/example/flickrfindr/ui/adapters/FlickrPhotoAdapter.kt
+++ FlickrFindr/app/src/main/java/com/example/flickrfindr/ui/adapters/FlickrPhotoAdapter.kt
@@ -1,4 +1,52 @@
 package com.example.flickrfindr.ui.adapters
 
-class FlickrPhotoAdapter {
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.ViewTarget
+import com.example.flickrfindr.R
+import com.example.flickrfindr.data.entities.BasePhoto
+import com.example.flickrfindr.data.entities.Photo
+import com.example.flickrfindr.databinding.FlickrListViewBinding
+
+class FlickrPhotoAdapter(private val basePhoto: BasePhoto?): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding  = FlickrListViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotosViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as PhotosViewHolder).bind(basePhoto?.photos?.photo?.get(position))
+    }
+
+    override fun getItemCount(): Int = basePhoto?.photos?.photo?.size ?: 0
+
+    class PhotosViewHolder(val flickrListViewBinding: FlickrListViewBinding): RecyclerView.ViewHolder(flickrListViewBinding.root){
+        fun bind(photo: Photo?){
+            with(flickrListViewBinding){
+                photo?.apply {
+                    loadImage(flickrPhoto, "https://live.staticflickr.com/${server}/${id}_${secret}_s.jpg")
+                    fldFlickrName.text = title
+                }
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun loadImage(view: ImageView, url: String): ViewTarget<ImageView, Bitmap> {
+            val circularProgressDrawable = CircularProgressDrawable(view.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+            return Glide.with(view).asBitmap().load(url).centerCrop()
+                .placeholder(circularProgressDrawable)
+                .error(R.drawable.ic_launcher_foreground)
+                .fallback(R.drawable.ic_launcher_foreground).into(view)
+        }
+    }
 }
