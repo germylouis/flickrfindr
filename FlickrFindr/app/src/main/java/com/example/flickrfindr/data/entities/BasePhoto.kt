71ec269436be1518d437a0b3cf895c1f6eeb593f
Germy Louis <glouis@imobile3.com>
2022-01-15 22:59:30 -0700

    Remove comments and fix package declaration.

--- FlickrFindr/app/src/main/java/com/example/flickrfindr/data/entities/BasePhoto.kt
+++ FlickrFindr/app/src/main/java/com/example/flickrfindr/data/entities/BasePhoto.kt
@@ -10,7 +10,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 For support, please feel free to contact me at https://www.linkedin.com/in/syedabsar */
 package com.example.flickrfindr.data.entities
 
-import Photos
 import kotlinx.serialization.SerialName
 
 data class BasePhoto(
--- FlickrFindr/app/src/main/java/com/example/flickrfindr/ui/fragments/PhotoFragment.java
+++ FlickrFindr/app/src/main/java/com/example/flickrfindr/ui/fragments/PhotoFragment.java
@@ -1,35 +1,28 @@
 package com.example.flickrfindr.ui.fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.example.flickrfindr.R;
 import com.example.flickrfindr.api.ImageViewExtension;
 import com.example.flickrfindr.databinding.PhotoFragmentBinding;
-
-import java.util.Objects;
 
 public class PhotoFragment extends Fragment {
     static String TAG = PhotoFragment.class.getName();
     private PhotoFragmentBinding binding;
 
     public PhotoFragment() {
-        // Required empty public constructor
         this.binding = null;
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = PhotoFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -37,9 +30,9 @@ public class PhotoFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if (getArguments() != null){
+        if (getArguments() != null) {
             ImageViewExtension.loadImage(binding.flickrPhoto, getArguments().getString("photo_url"));
-            binding.outsideImgView.setOnClickListener( l -> requireActivity().getSupportFragmentManager().popBackStack());
+            binding.outsideImgView.setOnClickListener(l -> requireActivity().getSupportFragmentManager().popBackStack());
         }
     }
 
